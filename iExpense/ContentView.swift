874865405a06d56492e2f3ca436b7b391c48dc71
mich balkany <askmitchb@gmail.com>
2023-11-08 14:53:41 -0500

    dismiss modifier (button) to dismiss the second page instead of swiping

--- iExpense/ContentView.swift
+++ iExpense/ContentView.swift
@@ -4,21 +4,38 @@
 //
 //  Created by Mich balkany on 11/8/23.
 //
-
+import Observation
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+struct SecondView: View{
+    @Environment(\.dismiss) var dismiss
+    let name: String
+    var body: some View{
+        Text("hello \(name)")
+        Button("Dismiss") {
+            dismiss()
         }
-        .padding()
+    }
+}
+
+
+struct ContentView: View {
+    @State private var showingSheet = false
+    
+    var body: some View {
+        Button("Show sheet") {
+            showingSheet.toggle()
+        }
+        .sheet(isPresented: $showingSheet) {
+            SecondView(name: "mich")
+        }
     }
 }
 
 #Preview {
     ContentView()
 }
+
+
+
+
