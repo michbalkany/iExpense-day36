e268ec1d0591a0d810a189459834a9e3f69f6850
mich balkany <askmitchb@gmail.com>
2023-11-09 15:04:15 -0500

    enter and save data after refreshing, after entering data and saving it the sheet closes,

---
+++ iExpense/AddView.swift
@@ -0,0 +1,18 @@
+//
+//  AddView.swift
+//  iExpense
+//
+//  Created by Mich balkany on 11/9/23.
+//
+
+import SwiftUI
+
+struct AddView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddView()
+}
