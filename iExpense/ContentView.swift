ea2b6c1e482b56552d839ce274ee301ac87de762
mich balkany <askmitchb@gmail.com>
2023-11-09 10:19:47 -0500

    each item in the list is now unique by giving a UUID and making our ExpensItem struct conform to teh Identifiable protocol

--- iExpense/ContentView.swift
+++ iExpense/ContentView.swift
@@ -7,36 +7,43 @@
 
 import SwiftUI
 
+struct ExpenseItem: Identifiable {
+    let id = UUID()
+    let name: String
+    let type: String
+    let amount: Double
+}
+
+// this carries all the data required for the main view
+@Observable
+class Expenses {
+    var items = [ExpenseItem]()
+}
+
 struct ContentView: View {
+    @State private var expenses = Expenses()
     
-    @State private var numbers = [Int]()
-    @State private var currentNumber = 1
     
-    var body: some View {
-        NavigationStack {
-            VStack {
-                List {
-                    // we only add the ForEach because adding the delete modifier only works with a ForEach as opposed to placing the rows in the list
-                    ForEach(numbers, id: \.self){
-                        Text("Row \($0)")
-                    }
-                    .onDelete(perform: removeRows)
-                }
-                Button("add new number"){
-                    numbers.append(currentNumber)
-                    currentNumber += 1
-                }
+var body: some View {
+    NavigationStack {
+        List {
+            ForEach(expenses.items) { item in
+                Text(item.name)
             }
-            .toolbar{
-                EditButton()
+            .onDelete(perform: removeItem)
+        }
+        .navigationTitle("iExpense")
+        .toolbar {
+            Button("Add Expense", systemImage:"plus") {
+                let expense = ExpenseItem(name: "Test", type: "personal", amount: 5)
+                expenses.items.append(expense)
+                }
             }
         }
     }
-    
-    func removeRows(at offSets: IndexSet) {
-        numbers.remove(atOffsets: offSets)
+    func removeItem(at offsets: IndexSet) {
+        expenses.items.remove(atOffsets: offsets)
     }
-    
 }
 
 #Preview {
