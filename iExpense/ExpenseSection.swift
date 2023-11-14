fc73000ac68b046e2f818791d7d01ed115faa382
mich balkany <askmitchb@gmail.com>
2023-11-14 10:20:01 -0500

    app is now complete with the view having two sections, all data is saved after adding new items

---
+++ iExpense/ExpenseSection.swift
@@ -0,0 +1,18 @@
+//
+//  ExpenseSection.swift
+//  iExpense
+//
+//  Created by Mich balkany on 11/14/23.
+//
+
+import SwiftUI
+
+struct ExpenseSection: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ExpenseSection()
+}
---
+++ iExpense/View-ExpenseStyle.swift
@@ -0,0 +1,8 @@
+//
+//  View-ExpenseStyle.swift
+//  iExpense
+//
+//  Created by Mich balkany on 11/13/23.
+//
+
+import Foundation
